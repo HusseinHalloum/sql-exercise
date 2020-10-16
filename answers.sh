d2d9e96483020e78f74a228bedf785e983f990a3
hussein <hseinhalloum00@gmail.com>
2020-10-16 14:07:16 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,9 @@
 5- INSERT INTO students(ID,name,Age,Gender,Points) VALUES (8,"Hussein",24,"M",200)
 6- UPDATE students SET Points=400 WHERE name="Basma"
 7- UPDATE students SET Points=100 WHERE name="Alex"
+
+Creating Table:
+1-CREATE TABLE "graduates" ( `ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `Name` TEXT NOT NULL UNIQUE, `Age` TEXT, `Gender` TEXT, `Points` INTEGER, "Graduation" Text )
+2- INSERT INTO graduates (name, Age, Gender,Points,Graduation) VALUES("Layal",18, "F", 350, "08/09/2018")
+3- DELETE FROM students WHERE name="Layal"
+
